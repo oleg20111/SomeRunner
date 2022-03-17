f774ffd1a29f3ee7b4b2227eb9416d7913a545f1
Oleg20111 <lusenkooleh1@gmail.com>
2022-03-17 23:21:59 +0200

    Add TMP and event Player-ScoreTMP

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private int _health;
 
+    public event UnityAction<int> HealthChanged;
+
+    private void Start()
+    {
+        HealthChanged?.Invoke(_health);
+    }
+
     public void ApplyDamage(int damage)
     {
         _health -= damage;
+        HealthChanged?.Invoke(_health);
 
         if (_health <= 0)
             Die();
